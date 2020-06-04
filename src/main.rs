2fd32926d477a29bd9138e00f92019e06611dfe5
Sean Myers <sean.myers@redhat.com>
2020-06-04 11:13:53 -0400

    Update main to be more readable.
    Functionality is the same, with the major change being comments, and
    also breaking up stream processing into its own function so that instead
    of setting a response string var to literal "Ok: ..." and "Err: ..."
    string, it returns Result enum values that get cast as the result
    strings in a single place later on. Because rust, and woo!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::io;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::str;
@@ -8,14 +7,57 @@ use std::time::Duration;
 
 mod async_fibber;
 
+// To enable bidirectional communications from withing threads, so
+// that all threads benefit from the fib cache, threads call into
+// the parent over an mpsc channel with a ThreadPhone. This has its
+// own mpsc channel embedded in it, as well as the requested fib index
+// to look up. The answer is calculated, and then sent back over the
+// ThreadPhone.tx channel. This admittedly results in cache lookups
+// being a potential bottleneck, but it's going to be a pretty wide
+// neck. I may (but probably won't) profile it at some point to see.
 struct ThreadPhone<T> {
     tx: mpsc::SyncSender<T>,
     n: usize,
 }
 
-fn main() -> io::Result<()> {
+// breaking this out into its own function was fun, if only to appreciate
+// what the actual type of the nested SyncSender is after resolving the
+// generic types. All this does is take the input buffer from the stream
+// and convert it to a Result.
+fn process_buffer(inner_tx: mpsc::SyncSender<ThreadPhone<Result<u64, String>>>, buffer: Vec<u8>) -> Result<String, String> {
+    // let the match nesting begin!
+    match str::from_utf8(&buffer) {
+        Ok(input) => {
+            let received = input.lines().next().unwrap();
+            match received.parse::<usize>() {
+                Ok(n) => {
+                    let (t_tx, t_rx) = mpsc::sync_channel(10);
+                    inner_tx.send(ThreadPhone { tx: t_tx, n: n }).unwrap();
+                    // the actual fibber answer, formatted to match the "interface"
+                    match t_rx.recv().unwrap() {
+                        Ok(n) => Ok(format!("{}", n)),
+                        Err(e) => Err(format!("{}", e))
+                    }
+                }
+                Err(_) => {
+                    Err(format!("'{}' is not an integer followed by a newline", received))
+                }
+            }
+        }
+        Err(_) => {
+            // Bro, I couldn't even pretend to make a utf8 string out of you sent
+            Err(format!("Bro I can't even read that"))
+        }
+    }
+}
+
+fn main() {
     let (tx, rx) = mpsc::sync_channel(10);
     let mut cache = async_fibber::fib_cache();
+    let addr = "0.0.0.0:1234";
+    // panic will ensue if the port is in use :D
+    let listener = TcpListener::bind(addr).unwrap();
+    println!("Listening at {}", addr);
 
     // hand over the cache and receiver to a single worker thread
     // connections are concurrent, but generating and caching fib values isn't
@@ -26,55 +68,40 @@ fn main() -> io::Result<()> {
         }
     });
 
-    let addr = "0.0.0.0:1234";
-    let listener = TcpListener::bind(addr)?;
-    println!("Listening at {}", addr);
-    Ok(for stream in listener.incoming() {
+    let mut incoming = listener.incoming();
+    // So I'll be honest here. I'm new to this whole "if/while let" thing. I'm pretty sure
+    // that what I'm saying is that "while junk coming off of this iterator is not a "None"
+    // option, and is not a "Some" option wrapping an "Err" result, expose "stream" in
+    // the looping scope as the unwrapped value. I'm not entirely sure what happens to
+    // all those "None" values and "Some(Err(e))" values, though. I *assume* they got
+    // dropped on the floor, and that I won't end up with some kind of stuck iterator.
+    // I expect to look back on this comment in a few years (or days? I dunno) and laugh,
+    // as this is certainly contained somewhere in the firehose of docs I consumed over
+    // the past few days. Could be that when the while let hits an Err then main() panics,
+    // which would be bad. I should probably test this in isolation, but probably won't.
+    while let Some(Ok(mut stream)) = incoming.next() {
+        // pass off incoming connections to a thread asap, at which point the mpsc
+        // channel + ThreadPhone is used to pass work up to the worker thread.
+        // Thread panics end up getting printed, which is fine for this example,
+        // so everything that doesn't end up generating an Ok/Err response to the
+        // socket is just .unwrap'd.
         let inner_tx = tx.clone();
         thread::spawn(move|| {
-            let mut stream = stream.unwrap().try_clone().unwrap();
+            let mut buffer = vec![0; 16];
+            let formatted_response: String;
             stream.set_read_timeout(Some(Duration::new(30, 0))).unwrap();
-            let mut buffer = vec![0; 4];
-            match stream.read(&mut buffer) {
-                Ok(_) => {}
-                Err(_) => {
-                    return;
+            stream.read(&mut buffer).unwrap();
+            match process_buffer(inner_tx, buffer) {
+                Ok(msg) => {
+                    formatted_response = format!("Ok: {}\n", msg);
+                }
+                Err(msg) => {
+                    formatted_response = format!("Err: {}\n", msg);
                 }
             }
-            let response: String;
-            // let the match nesting begin!
-            match str::from_utf8(&buffer) {
-                Err(_) => {
-                    response = format!("Err: Bro I can't even read that");
-                }
-                Ok(input) => {
-                    let received = input.lines().next().unwrap();
-                    match received.parse::<usize>() {
-                        Ok(n) => {
-                            let (t_tx, t_rx) = mpsc::sync_channel(10);
-                            match inner_tx.send(ThreadPhone { tx: t_tx, n: n }) {
-                                Ok(_) => {}
-                                Err(e) => {println!("{:?}", e);}
-                            }
-                            match t_rx.recv().unwrap() {
-                                Ok(n) => {
-                                    response = format!("Ok: {}", n);
-                                }
-                                Err(e) => {
-                                    response = format!("Err: {}", e);
-                                }
-                            }
-                        }
-                        Err(_) => {
-                            response = format!("'{}' is not an integer followed by a newline.", received);
-                        }
-                    }
-                    println!("wanted {}, replied {:?}", received, response);
-                }
-            }
-            // lazy slapping of newline on all responses sent back over the wire
-            let response = format!("{}\n", response);
-            stream.write(&response.as_bytes()).unwrap();
+            stream.write(&formatted_response.as_bytes()).unwrap();
+            // log what happened
+            println!("sent {}", formatted_response.trim());
         });
-    })
+    }
 }
